3334f2fcf287e6b26f76747b414ee34be79a8633
DK\dmsa <dmsa@sitecore.net>
2017-10-26 15:52:02 +0300

    Fix for issue #190641 - [IE][Edge]Borders of Sitecore controls cannot be resized in Experience Editor when Microsoft IE or Edge browser is used

---
+++ src/Sitecore.Support.190641/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.190641")]
+[assembly: AssemblyProduct("Sitecore.Support.190641")]
+[assembly: ComVisible(false)]
